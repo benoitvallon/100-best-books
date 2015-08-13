4e5f8792021d1805c4971ae0bdc7db80e613a8ef
Benoit VALLON <benoitvallon@gmail.com>
2015-08-13 15:31:30 -0400

    Adds an extract method and homepage

--- readme.md
+++ readme.md
@@ -2,3 +2,19 @@
 ==========================
 
 An website to play with the Bokklubben World Library which is a series of the 100 best classical books
+
+# Configuration
+
+In order to launch the project you must define a config file. To do so, export an `env` variable called `BOKKLUBBEN_SETTINGS` which define the path to your config file.
+
+```shell
+export BOKKLUBBEN_SETTINGS=settings.cfg
+```
+
+An example of your config file could be:
+
+```shell
+# configuration
+DEBUG = True
+ISBNDB_KEY = 'MY_FAKE_KEY'
+```
--- web.py
+++ web.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 from flask.ext.assets import Environment, Bundle
 import wikipedia
+import urllib2
+import pudb
+import re
+import json
+import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-assets = Environment(app)
-
-# assets = flask.ext.assets.Environment()
-assets.init_app(app)
-
+app.config.from_envvar('BOKKLUBBEN_SETTINGS', silent=True)
 
 assets = Environment(app)
 assets.url = app.static_url_path
@@ -18,21 +19,100 @@ assets.register('scss_all', scss)
 js = Bundle('jquery.js', 'bootstrap.min.js', 'custom.js', filters='jsmin', output='javascripts.js')
 assets.register('js_all', js)
 
-@app.route('/')
-def home():
-  bok = wikipedia.page("Bokklubben_World_Library")
+def requestWikipedia(page):
+  return wikipedia.page(page)
+
+def requestIsbndb(book):
+  pageName = book['title']
+  pageName = pageName.replace("_", " ")
+
+  if book['author'] != 'Unknow':
+    pageName = pageName + " " + book['author']
+  print book
+  print pageName
+  pageNameEncoded = urllib2.quote(pageName.encode('UTF-8'))
+  print 'Request for: ' + pageNameEncoded
+  isbndbXml = urllib2.urlopen("http://isbndb.com/api/v2/xml/" + app.config['ISBNDB_KEY'] + "/books?q=" + pageNameEncoded).read()
+
+  bestResult = getIsbndbBestResult(isbndbXml)
+
+
+  book['description'] = bestResult.find('physical_description_text').text
+  isbn10 = bestResult.find('isbn10').text
+  book['imageLink'] = "http://covers.openlibrary.org/b/isbn/" + isbn10 + ".jpg"
+
+  return book
+
+def getIsbndbBestResult(isbndbXml):
+  root = ET.fromstring(isbndbXml)
+  results = root.findall('data')
+
+  for result in results:
+    image = urllib2.urlopen("http://covers.openlibrary.org/b/isbn/" + result.find('isbn10').text + ".jpg")
+    print "http://covers.openlibrary.org/b/isbn/" + result.find('isbn10').text + ".jpg"
+
+    responseHeaders = image.info()
+    # is the result of the image load request a real image
+    if "content-type" in responseHeaders.keys():
+      print "image found"
+      # is there a description in the book
+      physicalDescriptionText = result.find('physical_description_text').text
+      print physicalDescriptionText
+      if physicalDescriptionText is not None and physicalDescriptionText != "":
+        print "description found"
+        reMatch = re.search(r'([0-9]*)\s*pages', physicalDescriptionText)
+        if reMatch:
+          print "pages in description: " + reMatch.group(1)
+          return result
+        else:
+          print "no pages description"
+        # return result
+      else:
+        print "no description"
+    else:
+      print "no image"
+
+  print 'All results processed'
+
+  return results[0]
+
+@app.route('/extract')
+def extract():
+  bok = requestWikipedia("Bokklubben_World_Library")
   htmlPage = BeautifulSoup(bok.html(), 'html.parser')
   table = htmlPage.select(".wikitable")[0]
   lines = table.select('tr')
 
   # we remove the first line of the table as it is the legend
-  lines = lines[1:]
-  titles = []
+  lines = lines[1:3]
+  books = []
   for line in lines:
-    cell = line.select('td')[0]
-    titles.append(cell.get_text().strip())
+    cells = line.select('td')
+    title = cells[0].get_text().strip()
+    link = "https://en.wikipedia.org" + cells[0].find('a').get('href').strip() if cells[0].find('a') else ''
+    author = cells[1].find('a').get_text().strip() if cells[1].find('a') else ''
+    book = dict(title=title, author=author, link=link)
+    book = requestIsbndb(book)
+    books.append(book)
 
-  return render_template('home.html', titles= titles)
+  formattedJson = json.dumps(books, sort_keys=True, indent=2, separators=(',', ': '))
+  # Open/close a file
+  fileOpen = open("books.json", "w")
+  fileData = fileOpen.write(formattedJson)
+  fileOpen.close()
+
+  return render_template('home.html', books= books)
+
+@app.route('/')
+def home():
+  # Open/close a file
+  fileOpen = open("books.json", "r")
+  fileData = fileOpen.read()
+  fileOpen.close()
+
+  books = json.loads(fileData)
+
+  return render_template('home.html', books= books)
 
 if __name__ == '__main__':
   app.run(debug=True)
