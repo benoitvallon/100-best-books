d0157f9a93ebbb094a8bafc47b18e0075ba76445
Benoit VALLON <benoitvallon@gmail.com>
2015-10-08 11:41:06 -0400

    Fixes transparent navbar on home

--- static/custom.js
+++ static/custom.js
@@ -1,1 +1,33 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  $('#bs-example-navbar-collapse-1').on('show.bs.collapse', function () {
+    var heightLimit = 10,
+        height = $(window).scrollTop();
+
+    if(height <= heightLimit) {
+      $('.navbar').removeClass('navbar-transparent');
+    }
+  });
+
+  $('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function () {
+    var heightLimit = 10,
+        height = $(window).scrollTop();
+
+    if(height <= heightLimit) {
+      $('.navbar').addClass('navbar-transparent');
+    }
+  });
+});
+
+$(window).scroll(function () {
+  var heightLimit = 100,
+      height = $(window).scrollTop();
+      navbarCollapsed = $('.navbar-collapse.in').length;
+
+  if(!navbarCollapsed) {
+    if(height > heightLimit) {
+      $('.navbar').removeClass('navbar-transparent');
+    } else {
+      $('.navbar').addClass('navbar-transparent');
+    }
+  }
+});
